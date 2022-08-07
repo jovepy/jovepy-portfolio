02c98227746dc24077daa90fdf9ba26193c88293
Rodrigo Jovê <datajove@gmail.com>
2022-08-07 01:06:04 -0300

    veremos

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,6 @@ export default function Home() {
           <button className="nav-link fs-6 fw-light  rounded-pill" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false" >Contato</button>
           
           {/* Menu lateral */}
-          
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-filter-right" viewBox="0 0 16 16" style={{color:"#FFDC4E"}} className='animate-bounce w-10 h-7'>
               <path d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5z"/>
