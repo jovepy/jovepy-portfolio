0a8b09f0402126815b823d593f9e65645c8a85a5
Rodrigo Jovê <datajove@gmail.com>
2022-08-07 01:14:00 -0300

    agr vai

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
           
           {/* Menu lateral */}
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-filter-right" viewBox="0 0 16 16" style={{color:"#FFDC4E"}} className='animate-bounce w-10 h-7'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor"  viewBox="0 0 16 16" style={{color:"#FFDC4E"}} className='animate-bounce w-10 h-7'>
               <path d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5z"/>
             </svg>
           </button>
