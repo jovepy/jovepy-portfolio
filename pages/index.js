8c758885aa872b1a9c5810847e2d1d9999f911a2
Rodrigo Jovê <datajove@gmail.com>
2022-08-09 22:17:56 -0300

    carregar mais rapido

--- pages/index.js
+++ pages/index.js
@@ -41,23 +41,7 @@ export default function Home() {
     <nav className="navbar fixed-top bg-white">
       <div className="container-fluid">
       <div className='text-center'>
-        <svg className='python' width="50px" height="50px" viewBox="0 0 256 255" version="1.1" style={{opacity:'100%'}}>
-          <defs>
-              <linearGradient x1="12.9593594%" y1="12.0393928%" x2="79.6388325%" y2="78.2008538%" id="linearGradient-1">
-                  <stop stopColor="#387EB8" offset="0%"></stop>
-                  <stop stopColor="#366994" offset="100%"></stop>
-              </linearGradient>
-              <linearGradient x1="19.127525%" y1="20.5791813%" x2="90.7415328%" y2="88.4290372%" id="linearGradient-2">
-                  <stop stopColor="#FFE052" offset="0%"></stop>
-                  <stop stopColor="#FFC331" offset="100%"></stop>
-              </linearGradient>
-          </defs>
-          <g>
-              <path className='pythonpath' id="blue-python" d="M126.915866,0.0722755491 C62.0835831,0.0722801733 66.1321288,28.1874648 66.1321288,28.1874648 L66.2044043,57.3145115 L128.072276,57.3145115 L128.072276,66.0598532 L41.6307171,66.0598532 C41.6307171,66.0598532 0.144551098,61.3549438 0.144551098,126.771315 C0.144546474,192.187673 36.3546019,189.867871 36.3546019,189.867871 L57.9649915,189.867871 L57.9649915,159.51214 C57.9649915,159.51214 56.8001363,123.302089 93.5968379,123.302089 L154.95878,123.302089 C154.95878,123.302089 189.434218,123.859386 189.434218,89.9830604 L189.434218,33.9695088 C189.434218,33.9695041 194.668541,0.0722755491 126.915866,0.0722755491 L126.915866,0.0722755491 L126.915866,0.0722755491 Z M92.8018069,19.6589497 C98.9572068,19.6589452 103.932242,24.6339846 103.932242,30.7893845 C103.932246,36.9447844 98.9572068,41.9198193 92.8018069,41.9198193 C86.646407,41.9198239 81.6713721,36.9447844 81.6713721,30.7893845 C81.6713674,24.6339846 86.646407,19.6589497 92.8018069,19.6589497 L92.8018069,19.6589497 L92.8018069,19.6589497 Z" fill="url('#linearGradient-1')"></path>
-
-              <path className='pythonpath' id="yellow-python" d="M128.757101,254.126271 C193.589403,254.126271 189.540839,226.011081 189.540839,226.011081 L189.468564,196.884035 L127.600692,196.884035 L127.600692,188.138693 L214.042251,188.138693 C214.042251,188.138693 255.528417,192.843589 255.528417,127.427208 C255.52844,62.0108566 219.318366,64.3306589 219.318366,64.3306589 L197.707976,64.3306589 L197.707976,94.6863832 C197.707976,94.6863832 198.87285,130.896434 162.07613,130.896434 L100.714182,130.896434 C100.714182,130.896434 66.238745,130.339138 66.238745,164.215486 L66.238745,220.229038 C66.238745,220.229038 61.0044225,254.126271 128.757101,254.126271 L128.757101,254.126271 L128.757101,254.126271 Z M162.87116,234.539597 C156.715759,234.539597 151.740726,229.564564 151.740726,223.409162 C151.740726,217.253759 156.715759,212.278727 162.87116,212.278727 C169.026563,212.278727 174.001595,217.253759 174.001595,223.409162 C174.001618,229.564564 169.026563,234.539597 162.87116,234.539597 L162.87116,234.539597 L162.87116,234.539597 Z" fill="url('#linearGradient-2')"></path>
-          </g>
-      </svg>
+      <Image className="img-fluid text-center" loader={myLoader} src='./python.svg' alt="Criamos a sua solulçao" width={150} height={50} />
     </div>
         <a className="navbar-brand" href="#"></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
@@ -201,7 +185,7 @@ export default function Home() {
                                               </div>
                                           </div>
                             <div>
-                            <h3 className="fw-light fs-6">Extração de dados</h3>
+                            <h3 className="fw-light fs-5">Extração de dados</h3>
                             <p className="fw-light fs-6 mb-0">Extração, limpeza, estruturação e manutenção de dados através de extrações automatizadas de fontes e formatos diversos - WEB, PDF, Imagens.</p>
                               <div className="progress opacity-50">
                                 <div className="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" aria-label="Animated striped example" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style={{width: "95%"}}></div>
@@ -232,7 +216,7 @@ export default function Home() {
                                   </div>
                                   <div className='border-top'></div>
                                   <div className="text-center d-grid gap-2 m-4">
-                                    <button  type="button" className='btn btn-outline-warning' data-bs-toggle="modal" data-bs-target="#exampleModal1" style={{color:'white',background:'#3770A0'}}>Sobre</button>  
+                                    <button  type="button" className='btn' data-bs-toggle="modal" data-bs-target="#exampleModal1" style={{color:'white',background:'#3770A0'}}>Sobre</button>  
                                     <button className="btn" type="button" onClick={() => Router.push('https://api.whatsapp.com/send?phone=+5583998775808&text=Olá!!')} style={{color:'white',background:'#3770A0'}}>Envie-me uma mensagem</button>
                                 </div>
                               </div>
@@ -252,8 +236,11 @@ export default function Home() {
                             Tenha acesso ao meu Notion para aprender programação e economia de forma simples e prática.
                           </div>
                           <div className="modal-footer">
+                          <div className='border-top'></div>
+                          <div className="text-center d-grid gap-2 m-4">
                           <button className="btn" type="button" onClick={() => Router.push('https://tender-silica-d85.notion.site/Jove-py-a23ca3b771e345649f73c965803197d5')} style={{color:'white',background:'#3770A0'}}>Ver Notion</button>
                           <button className="btn" type="button" onClick={() => Router.push('https://api.whatsapp.com/send?phone=+5583998775808&text=Olá!!')} style={{color:'white',background:'#3770A0'}}>Envie-me uma mensagem</button>
+                          </div>
                           </div>
                         </div>
                       </div>
