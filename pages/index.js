1e73b2a73c0fcbbc03d7a0f3d14d43e5b41ee3ae
Rodrigo Jovê <datajove@gmail.com>
2022-08-10 19:35:36 -0300

    veremos

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ import ReactDOM from 'react-dom';
 import Script from 'next/script'
 
 
-
 const myLoader = ({ src, width, quality }) => {
   return `./${src}?w=${width}&q=${quality || 75}`
 }
@@ -43,7 +42,23 @@ export default function Home() {
     <nav className="navbar fixed-top bg-white">
       <div className="container-fluid">
       <div className='text-center'>
-      <Image className="img-fluid text-center" loader={myLoader} src='./python.png' alt="Criamos a sua solulçao" width={150} height={50} />
+      <svg width="50px" height="50px" className='python' viewBox="0 0 255 255" version="1.1">
+    <defs>
+        <linearGradient x1="12.9593594%" y1="12.0393928%" x2="79.6388325%" y2="78.2008538%" id="linearGradient-1">
+            <stop stop-color="#387EB8" offset="0%"></stop>
+            <stop stop-color="#366994" offset="100%"></stop>
+        </linearGradient>
+        <linearGradient x1="19.127525%" y1="20.5791813%" x2="90.7415328%" y2="88.4290372%" id="linearGradient-2">
+            <stop stop-color="#FFE052" offset="0%"></stop>
+            <stop stop-color="#FFC331" offset="100%"></stop>
+        </linearGradient>
+    </defs>
+    <g>
+        <path id="blue-python" d="M126.915866,0.0722755491 C62.0835831,0.0722801733 66.1321288,28.1874648 66.1321288,28.1874648 L66.2044043,57.3145115 L128.072276,57.3145115 L128.072276,66.0598532 L41.6307171,66.0598532 C41.6307171,66.0598532 0.144551098,61.3549438 0.144551098,126.771315 C0.144546474,192.187673 36.3546019,189.867871 36.3546019,189.867871 L57.9649915,189.867871 L57.9649915,159.51214 C57.9649915,159.51214 56.8001363,123.302089 93.5968379,123.302089 L154.95878,123.302089 C154.95878,123.302089 189.434218,123.859386 189.434218,89.9830604 L189.434218,33.9695088 C189.434218,33.9695041 194.668541,0.0722755491 126.915866,0.0722755491 L126.915866,0.0722755491 L126.915866,0.0722755491 Z M92.8018069,19.6589497 C98.9572068,19.6589452 103.932242,24.6339846 103.932242,30.7893845 C103.932246,36.9447844 98.9572068,41.9198193 92.8018069,41.9198193 C86.646407,41.9198239 81.6713721,36.9447844 81.6713721,30.7893845 C81.6713674,24.6339846 86.646407,19.6589497 92.8018069,19.6589497 L92.8018069,19.6589497 L92.8018069,19.6589497 Z" fill="url('#linearGradient-1')" className='pythonpath'></path>
+
+        <path id="yellow-python" d="M128.757101,254.126271 C193.589403,254.126271 189.540839,226.011081 189.540839,226.011081 L189.468564,196.884035 L127.600692,196.884035 L127.600692,188.138693 L214.042251,188.138693 C214.042251,188.138693 255.528417,192.843589 255.528417,127.427208 C255.52844,62.0108566 219.318366,64.3306589 219.318366,64.3306589 L197.707976,64.3306589 L197.707976,94.6863832 C197.707976,94.6863832 198.87285,130.896434 162.07613,130.896434 L100.714182,130.896434 C100.714182,130.896434 66.238745,130.339138 66.238745,164.215486 L66.238745,220.229038 C66.238745,220.229038 61.0044225,254.126271 128.757101,254.126271 L128.757101,254.126271 L128.757101,254.126271 Z M162.87116,234.539597 C156.715759,234.539597 151.740726,229.564564 151.740726,223.409162 C151.740726,217.253759 156.715759,212.278727 162.87116,212.278727 C169.026563,212.278727 174.001595,217.253759 174.001595,223.409162 C174.001618,229.564564 169.026563,234.539597 162.87116,234.539597 L162.87116,234.539597 L162.87116,234.539597 Z" fill="url('#linearGradient-2')" className='pythonpath'></path>
+    </g>
+</svg>
     </div>
         <a className="navbar-brand" href="#"></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
@@ -56,36 +71,37 @@ export default function Home() {
             <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div className="offcanvas-body" style={{background:'#3770A0'}}>
-            <ul className="navbar-nav justify-content-end flex-grow-2 pe-3">
+            <ul className="navbar-nav justify-content-end flex-grow-2 pe-3 ">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#" style={{color:'white'}}><h5 className='text-start fs-4'>Inicio</h5></a>
+                <a className="nav-link active" aria-current="page" href="#" style={{color:'white'}}><div className='border-bottom mb-0'><h5 className='text-center fs-4 mb-0'>Inicio</h5></div></a>
+                
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#" style={{color:'white'}}><h5 className='text-start fs-4'>Serviços</h5></a>
+                <a className="nav-link" href="#" style={{color:'white'}}><div className='border-bottom mb-0'><h5 className='text-center fs-4 mb-0'>Serviços</h5></div></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#" style={{color:'white'}}><h5 className='text-start fs-4'>Projetos</h5></a>
+                <a className="nav-link" href="#" style={{color:'white'}}><div className='border-bottom mb-0'><h5 className='text-center fs-4 mb-0'>Projetos</h5></div></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#" style={{color:'white'}}><h5 className='text-start fs-4'>Artigos</h5></a>
+                <a className="nav-link" href="#" style={{color:'white'}}><div className='border-bottom mb-0'><h5 className='text-center fs-4 mb-0'>Artigos</h5></div></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#" style={{color:'white'}}><h5 className='text-start fs-4'>Contato</h5></a>
+                <a className="nav-link" href="#" style={{color:'white'}}><div className='border-bottom mb-0'><h5 className='text-center fs-4 mb-0'>Contato</h5></div></a>
               </li>
             </ul>
             
             <div className='container text-center'>
             <div className="grid grid-cols-1 gap-1 place-items-center h-32">
                 <div className="btn-group text-center" role="group">
-                    <svg xmlns="http://www.w3.org/2000/svg"  width="45" height="45" fill="currentColor" className="bi bi-linkedin m-2" viewBox="0 0 16 16" type="button" onClick={() => Router.push('https://www.linkedin.com/in/rodrigojove/')} style={{color:'white'}}>
+                    <svg xmlns="http://www.w3.org/2000/svg"  width="60" height="60" fill="currentColor" className="bi bi-linkedin m-4" viewBox="0 0 16 16" type="button" onClick={() => Router.push('https://www.linkedin.com/in/rodrigojove/')} style={{color:'white'}}>
                     
                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
                   </svg>
                   
-                  <svg  xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" className="bi bi-whatsapp m-2" viewBox="0 0 16 16" type="button" onClick={() => Router.push('https://api.whatsapp.com/send?phone=+5583998775808&text=Olá!!')} style={{color:'white'}}>
+                  <svg  xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-whatsapp m-4" viewBox="0 0 16 16" type="button" onClick={() => Router.push('https://api.whatsapp.com/send?phone=+5583998775808&text=Olá!!')} style={{color:'white'}}>
                   <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/></svg>
 
-                  <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" className="bi bi-github m-2" viewBox="0 0 16 16" type="button" onClick={() => Router.push('https://github.com/jovepy')} style={{color:'white'}}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-github m-4" viewBox="0 0 16 16" type="button" onClick={() => Router.push('https://github.com/jovepy')} style={{color:'white'}}>
                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>  
               </div>
             </div>
@@ -107,9 +123,10 @@ export default function Home() {
             <Image className="img-fluid rounded-circle text-center" loader={myLoader} src='./perfil.png' alt="Criamos a sua solulçao" width={200} height={200} /> 
             </div>
             </div>
-            <div className="col-lg-8">
+            <div className="col-8">
               <div className="card-body align-middle">
-                <h5 className="text-start fs-1 fw-semibold" >Rodrigo Jovê</h5>
+                <h5 className="text-start fs-1 fw-semibold mb-0" >Rodrigo Jovê</h5>
+                  <small className='text-star fs-6 fw-light mb-0'>Jove.py</small>
                   <p className="fw-light card-text text-start align-middle">
                   <ul className="list-group list-group-flush">
                       <li className="list-group list-group-flush">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     // if you have other folder, add its path too, like this:
     "./components/**/*.{js,ts,jsx,tsx}",
+
+    "./src/**/*.{html,js}",
   ],
   theme: {
     extend: {},
